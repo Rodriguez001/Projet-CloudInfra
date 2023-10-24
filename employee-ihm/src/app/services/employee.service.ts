ec27a4e522eb2e9f8d681a1d3753642b4f7788c6
rodriguez001 <kamgainrodrigue3@gmail.com>
2023-10-24 20:57:12 +0000

    add frontend

---
+++ employee-ihm/src/app/services/employee.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EmployeeService {
+  host_ip: string = window.location.hostname || 'localhost';
+  baseUrl: string = 'http://' + this.host_ip + ':9090';
+
+  constructor(private _http: HttpClient) {}
+
+  addEmployee(data: any): Observable<any> {
+    return this._http.post(`${this.baseUrl}/employees`, data);
+  }
+
+  updateEmployee(id: number, data: any): Observable<any> {
+    return this._http.put(`${this.baseUrl}/employee/${id}`, data);
+  }
+
+  getEmployeeList(): Observable<any> {
+    return this._http.get(`${this.baseUrl}/employees`);
+  }
+
+  deleteEmployee(id: number): Observable<any> {
+    return this._http.delete(`${this.baseUrl}/employee/${id}`);
+  }
+}
